f4babaad62ed6525a86affe9d601169e1973d586
Carl Olson <carlolson@users.noreply.github.com>
2017-04-25 13:21:16 -0500

    change pair implementation, same as tink library

--- src/Monads.hx
+++ src/Monads.hx
@@ -2,7 +2,7 @@ class Monads {
     private function new() {}
 
     public static function makePair<T, R>(left:T, right:R):Pair<T, R> {
-	return {left: left, right: right};
+	return new Pair(left, right);
     }
 
     public static function just<T>(value:T):Maybe<T> {
--- src/Pair.hx
+++ src/Pair.hx
@@ -1,1 +1,15 @@
-typedef Pair<T, R> = { left:T, right:R };
+typedef PairImpl<T, R> = { left:T, right:R };
+
+abstract Pair<T, R>(PairImpl<T, R>) {
+    inline public function new(left:T, right:R)
+	this = {left: left, right: right};
+
+    public var left(get, never):T;
+    public var right(get, never):R;
+
+    inline private function get_left():T
+	return this.left;
+
+    inline private function get_right():R
+	return this.right;
+}
